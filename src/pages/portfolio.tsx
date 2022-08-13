93ebb1c2c803d93f2554a3af2ed6d0c3974ed8ea
İbrahim Telman <ibrahimtlmn@gmail.com>
2022-08-13 16:14:32 +0300

    feat: create portfolio summary section

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,11 @@
+import PortfolioSummary from '../sections/PortfolioSummary'
+
+const Portfolio = () => {
+  return (
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <PortfolioSummary />
+    </div>
+  )
+}
+
+export default Portfolio
