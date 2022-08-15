cda87190698efe6f7bf90f142848eb68f29944cb
İbrahim Telman <ibrahimtlmn@gmail.com>
2022-08-15 21:24:18 +0200

    fix: missing property field

--- src/sections/Positions/index.tsx
+++ src/sections/Positions/index.tsx
@@ -44,7 +44,7 @@ const PositionsSection = () => {
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
       >
         {events.map((event) => (
-          <EventCard key={event} event={event} />
+          <EventCard key={event.id} event={event} />
         ))}
       </ul>
     </div>
