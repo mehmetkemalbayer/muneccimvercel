0aba04ec700acbb74f141a004504df96f3e64b92
mkb <kemal.bayer@gmail.com>
2022-11-18 17:58:55 +0300

    calls service in positions

--- src/sections/Positions/index.tsx
+++ src/sections/Positions/index.tsx
@@ -1,7 +1,23 @@
 import events from '@/mock/eventsMock'
 import EventCard from '@/components/EventCard'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const PositionsSection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    refreshData();
+  }, []);
+
+  const refreshData = () => {
+    axios.get('https://0lwihue84b.execute-api.eu-central-1.amazonaws.com/client/v1/events').then(response => {
+      // console.log(response.data);
+      setData(response.data);
+    });
+  }
+
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-0 lg:px-0 py-4">
       <h2 className="text-xl mb-2">Traded Events</h2>
@@ -9,8 +25,8 @@ const PositionsSection = () => {
         role="list"
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
       >
-        {events.map((event) => (
-          <EventCard key={event.id} event={event} />
+        {data.map((event) => (
+          <EventCard key={event['id']} event={event} />
         ))}
       </ul>
     </div>
